7c22c19794897da8d7b69be3cbe17139aca10036
Robert Wolf <wolfrob35@gmail.com>
2018-09-01 19:54:38 -0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi!</h1>
+    <p>My name is Robert Wolf and I love Gatbsy!</p>
+    <Link to="http://robertwolf.cz/">See my portfolio</Link>
+  </div>
+)
+
+export default IndexPage
